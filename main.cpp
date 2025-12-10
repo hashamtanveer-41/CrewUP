6b470f8ca7c848d5353c52d041871850125b08b4
Hasham Tanveer <hashamtanvr41@gmail.com>
2025-12-10 07:25:14 +0500

    Entering the name of Members logic was defined

--- main.cpp
+++ main.cpp
@@ -2,7 +2,36 @@
 
 using namespace std;
 
+// Functions Prototypes
+void nameOfMembers(string arr[], int len);
+void percentageOfMembers(int percentage);
+struct Members{
+    string name[5];
+    int percentage;
+    int exp;
+};
 int main(){
-    
+    bool run = true;
+    int choice, num = 0;
+    Members members;
+    cout<<"Please the required action: "<<endl;
+    cin>>choice;
+    while (run) {
+        switch (choice) {
+            case 1:
+                cout<<"Enter the number of members: ";
+                cin>>num;
+                nameOfMembers(members.name ,num);
+        }
+    }
     return 0;
+}
+void nameOfMembers(string arr[], int len) {
+    Members members;
+    arr = members.name;
+    //int len =arr->length();
+    for (int i= 0; i< len; i++) {
+        cout<<"Enter the name of the member "<<i+1<<": "<<endl;
+        cin>>arr[i];
+    }
 }
